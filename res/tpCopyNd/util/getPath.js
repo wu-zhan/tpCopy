0671258684ef373da32b86d57f97aebd3c15ee45
ZHAN.WU <ZHAN.WU@hand-china.com>
2020-07-25 15:35:24 +0800

    [FIX] 修复json引入路基错误

--- res/tpCopyNd/util/getPath.js
+++ res/tpCopyNd/util/getPath.js
@@ -2,17 +2,18 @@ const path = require('path')
 const fs = require('fs');
 const {promisify} = require('./promiseify.js');
 const readFilePro = promisify(fs.readFile);
+const saveJSON = require('../../saveJSON.json')
 
 // 获取config路径
 const getPath = async function () {
-    const saveJSON = await readFilePro(path.join(__dirname, '../../saveJSON.json'))
-    const {initTemplate, rplConfig} = require(JSON.parse(saveJSON).path + '/config.js');
+    // const saveJSON = await readFilePro(path.join(__dirname, '../../saveJSON.json'))
+    const {initTemplate, rplConfig} = require(saveJSON.path + '/config.js');
 
     // 项目res跟路径
     const getDIRPath = (...dir) => path.join(...dir)
     // 命令执行路径下的创建的新目录的路径
     const getCWDPath = (...dir) => path.join(process.cwd(), ...dir)
-    const templateDIRPath = JSON.parse(saveJSON).path + '/template'
+    const templateDIRPath = saveJSON.path + '/template'
     return {initTemplate, templateDIRPath, getDIRPath, getCWDPath, rplConfig}
 }
 
--- tpCopyConfig/config.js
+++
@@ -1,7 +0,0 @@
-const config = {
-    initTemplate: 'template1',
-    rplConfig: [ // 替换规则对应值
-        ['_xxxName_', 'dirName']
-    ]
-}
-module.exports = config
